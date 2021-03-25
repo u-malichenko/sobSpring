3b120deda9eb825ec6506db5eabd75bf974b81ff
u.malichenko <u.malichenko@gmail.com>
2021-03-25 20:49:53 +0300

    dz6

---
+++ src/main/java/ru/malichenko/sobSpring/SobSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.malichenko.sobSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobSpringApplication.class, args);
+	}
+
+}
